b7aa81f5ff724dae45824390c07d33adf05a905b
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-16 15:23:00 -0300

    Update ConfiguracaoView.py
    Refiz o código utilizando elementos de GUI com a biblioteca pygame_gui

--- versao_final/ConfiguracaoView.py
+++ versao_final/ConfiguracaoView.py
@@ -1,56 +1,54 @@
-from Configuracoes import Configuracoes
-from TelaJogo import TelaJogo
+from View import View
 import pygame
-import sys
+import pygame_gui
 
-class ConfiguracaoView:
+
+class ConfiguracaoView(View):
     def __init__(self):
-        self.__tela = TelaJogo()
-        self.__configuracoes = Configuracoes()
+        super().__init__()
+        self.__botao_continuar = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((self.posicao_botao[0], self.tela.altura - self.tamanho_botao[1] - 20), self.tamanho_botao),
+                                            text='Continuar',
+                                            manager=self.manager)
+
+        self.posicao_botao = (50, self.tela.altura/2 + 60)
+        self.tamanho_botao = (50, 50)
+
+        self.__botao_diminuir = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(self.posicao_botao, self.tamanho_botao),
+                                            text='-',
+                                            manager=self.manager)
+        self.__botao_aumentar = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((self.posicao_botao[0] + self.tamanho_botao[0] + 10, self.posicao_botao[1]), self.tamanho_botao),
+                                            text='+',
+                                            manager=self.manager) 
+
+        self.botoes = [self.__botao_diminuir, self.__botao_aumentar, self.__botao_continuar]
+        self.atualizar()
+
         self.__imagem_som = pygame.transform.scale(pygame.image.load('volume.png'), (150,150))
+        self.__fonte_mensagem = pygame.font.SysFont('comicssans', 100)
+        self.__fonte_volume = pygame.font.SysFont('comicssans', 250)
+
+    @property
+    def botao_diminuir(self):
+        return self.__botao_diminuir
+
+    @property
+    def botao_aumentar(self):
+        return self.__botao_aumentar
+
+    @property
+    def botao_continuar(self):
+        return self.__botao_continuar
+
+    def textos(self, total_volume: int):
+        configuracao = self.__fonte_mensagem.render("Configurações", True, (255,255,255))
+        self.tela.janela.blit(configuracao, (self.tela.largura / 5, 50))
+
+        volume = '-' * total_volume
+        qtd_volume = self.__fonte_volume.render(volume, True, (255,255,255))
+        self.tela.janela.blit(qtd_volume, (220, (self.tela.altura/2) - 110))
     
-    def mensagens(self):
-        fonte_mensagem = pygame.font.SysFont('comicssans', 100)
-        fonte_botao = pygame.font.SysFont('comicssans', 70)
-        fonte_volume = pygame.font.SysFont('comicssans', 250)
-
-        configuracao = fonte_mensagem.render("Configurações", True, (255,255,255))
-        self.__tela.janela.blit(configuracao, (self.__tela.largura / 5, 50))
-
-        botao_iniciar = fonte_botao.render("Voltar", True, (0,0,0))
-        self.__tela.janela.blit(botao_iniciar, ((self.__tela.largura / 2) - 80, self.__tela.altura - 100))
-
-        volume = '-' * self.__configuracoes.volume_jogo
-        qtd_volume = fonte_volume.render(volume, True, (255,255,255))
-        self.__tela.janela.blit(qtd_volume, (220, (self.__tela.altura/2) - 110))
-    
-    def botoes(self):
-        pygame.draw.rect(self.__tela.janela, (0,0,0), [50, (self.__tela.altura/2) + 80, 60, 20])
-        pygame.draw.rect(self.__tela.janela, (0,0,0), [130, (self.__tela.altura/2) + 80, 60, 20])
-        pygame.draw.rect(self.__tela.janela, (0,0,0), [150, (self.__tela.altura/2) + 60, 20, 60])
-
-        pygame.draw.rect(self.__tela.janela, (128,128,128), [(self.__tela.largura / 4) - 80, self.__tela.altura - 120, self.__tela.largura - 250, 80])
-
-    def rodar(self):
-        menu = True
-
-        while menu:
-            mouse = pygame.mouse.get_pos()
-
-            self.__tela.mostrar_fundo()
-            self.__tela.janela.blit(self.__imagem_som, (50, (self.__tela.altura/2) - 100))
-            self.botoes()
-            self.mensagens()
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if 110 > mouse[0] > 60 and (self.__tela.altura/2) + 100 > mouse[1] > (self.__tela.altura/2) + 80:
-                        self.__configuracoes.diminuir_volume()                    
-                    elif 190 > mouse[0] > 130 and (self.__tela.altura/2) + 120 > mouse[1] > (self.__tela.altura/2) + 60:
-                        self.__configuracoes.aumentar_volume()
-                    elif ((self.__tela.largura / 4) - 80 + self.__tela.largura - 250) > mouse[0] > (self.__tela.largura / 4) - 80 and self.__tela.altura - 40 > mouse[1] > self.__tela.altura - 120:
-                        menu = False
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+    def window(self, volume: int):
+        self.tela.mostrar_fundo()
+        self.tela.janela.blit(self.__imagem_som, (50, (self.tela.altura/2) - 100))
+        self.textos(volume)
+        super().window()
