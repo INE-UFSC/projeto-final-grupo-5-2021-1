2d514da63a4c59990c571cfdaef71fcf283c67bb
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-16 15:22:05 -0300

    Update PausaView.py
    Refiz o código utilizando elementos de GUI com a biblioteca pygame_gui

--- versao_final/PausaView.py
+++ versao_final/PausaView.py
@@ -1,48 +1,33 @@
-from Menu import MenuJogo
+from View import View
 import pygame
-#import pygame_gui
-from TelaJogo import TelaJogo
-import sys
+import pygame_gui
 
 
-class PausaView:
+class PausaView(View):
     def __init__(self):
-        self.tela = TelaJogo()
-        self.__tamanho_botao = (self.tela.largura - 250, 80)
-        self.__posicao_botao = ((self.tela.largura / 4) - 80, 150)
-    
-    def mensagens(self):
-        fonte_botao = pygame.font.SysFont('comicssans', 70)
+        super().__init__()
+        self.__botao_continuar = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(self.posicao_botao, self.tamanho_botao),
+                                            text='Continuar',
+                                            manager=self.manager)
+        self.__botao_configurar = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((self.posicao_botao[0], self.posicao_botao[1] + self.tamanho_botao[1] + 20), self.tamanho_botao),
+                                            text='Configuração',
+                                            manager=self.manager)
+        self.__botao_menu = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((self.posicao_botao[0], self.posicao_botao[1] + 2*(self.tamanho_botao[1] + 20)), self.tamanho_botao),
+                                            text='Menu',
+                                            manager=self.manager)
 
-        botao_iniciar = fonte_botao.render("Continuar", True, (0,0,0))
-        self.tela.janela.blit(botao_iniciar, (self.__posicao_botao[0] + 150, self.__posicao_botao[1] + 15))
+        self.botoes = [self.__botao_continuar, self.__botao_configurar, self.__botao_menu]
+        self.atualizar()
 
-        botao_configuracao = fonte_botao.render("Configurações", True, (0,0,0))
-        self.tela.janela.blit(botao_configuracao, (self.__posicao_botao[0] + 100, self.__posicao_botao[1] + self.__tamanho_botao[1] + 65))
+    @property
+    def botao_continuar(self):
+        return self.__botao_continuar
 
-        botao_sair = fonte_botao.render("Menu", True, (0,0,0))
-        self.tela.janela.blit(botao_sair, (self.__posicao_botao[0] + 200, self.__posicao_botao[1] + 2 * (self.__tamanho_botao[1] + 50) + 15))
-    
-    def botoes(self):
-        pygame.draw.rect(self.tela.janela, (0,255,0), [self.__posicao_botao[0], self.__posicao_botao[1], self.__tamanho_botao[0], self.__tamanho_botao[1]])
-        pygame.draw.rect(self.tela.janela, (128,128,128), [self.__posicao_botao[0], self.__posicao_botao[1] + self.__tamanho_botao[1] + 50, self.__tamanho_botao[0], self.__tamanho_botao[1]])
-        pygame.draw.rect(self.tela.janela, (255,0,0), [self.__posicao_botao[0], self.__posicao_botao[1] + 2 * (self.__tamanho_botao[1] + 50), self.__tamanho_botao[0], self.__tamanho_botao[1]])
-    
-    def rodar(self):
-        self.botoes()
-        self.mensagens()
-        
-        #pygame.display.update()
-        mouse = pygame.mouse.get_pos()
+    @property
+    def botao_configurar(self):
+        return self.__botao_configurar
 
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.__posicao_botao[0] + self.__tamanho_botao[0] > mouse[0] > self.__posicao_botao[0] and self.__posicao_botao[1] + self.__tamanho_botao[1] > mouse[1] > self.__posicao_botao[1]:
-                    return False
-                elif self.__posicao_botao[0] + self.__tamanho_botao[0] > mouse[0] > self.__posicao_botao[0] and self.__posicao_botao[1] + 2 * self.__tamanho_botao[1] + 50 > mouse[1] > self.__posicao_botao[1] + self.__tamanho_botao[1] + 50:
-                    pass
-                elif self.__posicao_botao[0] + self.__tamanho_botao[0] > mouse[0] > self.__posicao_botao[0] and self.__posicao_botao[1] + 3 * self.__tamanho_botao[1] + 100 > mouse[1] > self.__posicao_botao[1] + 2 * (self.__tamanho_botao[1] + 50):
-                    MenuJogo().menu()
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+    @property
+    def botao_menu(self):
+        return self.__botao_menu
+  
