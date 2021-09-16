5a13c2371c00282c177759a72ef860512352d94b
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-16 15:21:00 -0300

    Update FimJogoView.py
    Refiz o código utilizando elementos de GUI com a biblioteca pygame_gui

--- versao_final/FimJogoView.py
+++ versao_final/FimJogoView.py
@@ -1,45 +1,27 @@
+from View import View
 import pygame
-#import pygame_gui
-from TelaJogo import TelaJogo
-import sys
+import pygame_gui
 
 
-class FimJogoView:
+class FimJogoView(View):
     def __init__(self):
-        self.tela = TelaJogo()
-        self.__tamanho_botao = (self.tela.largura - 250, 80)
-        self.__posicao_botao = ((self.tela.largura / 4) - 80, 100)
-    
-    def mensagens(self):
-        fonte_mensagem = pygame.font.SysFont('comicssans', 100)
-        fonte_botao = pygame.font.SysFont('comicssans', 70)
+        super().__init__()
+        self.posicao_botao = ((self.tela.largura / 4) - 80, self.tela.altura/2 - 60)
 
-        fim_jogo = fonte_mensagem.render("Você perdeu!", True, (255,255,255))
-        self.tela.janela.blit(fim_jogo, (self.tela.largura / 5 + 10, 80))
+        self.__botao_recomecar = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(self.posicao_botao, self.tamanho_botao),
+                                            text='Recomeçar',
+                                            manager=self.manager)
+        self.__botao_sair = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((self.posicao_botao[0], self.posicao_botao[1] + self.tamanho_botao[1] + 20), self.tamanho_botao),
+                                            text='Sair',
+                                            manager=self.manager) 
 
-        botao_configuracao = fonte_botao.render("Recomeçar", True, (0,0,0))
-        self.tela.janela.blit(botao_configuracao, (self.__posicao_botao[0] + 140, self.__posicao_botao[1] + self.__tamanho_botao[1] + 65))
+        self.botoes = [self.__botao_recomecar, self.__botao_sair]
+        self.atualizar()
 
-        botao_sair = fonte_botao.render("Sair", True, (0,0,0))
-        self.tela.janela.blit(botao_sair, (self.__posicao_botao[0] + 220, self.__posicao_botao[1] + 2 * (self.__tamanho_botao[1] + 50) + 15))
-    
-    def botoes(self):
-        pygame.draw.rect(self.tela.janela, (0,255,0), [self.__posicao_botao[0], self.__posicao_botao[1] + self.__tamanho_botao[1] + 50, self.__tamanho_botao[0], self.__tamanho_botao[1]])
-        pygame.draw.rect(self.tela.janela, (255,0,0), [self.__posicao_botao[0], self.__posicao_botao[1] + 2 * (self.__tamanho_botao[1] + 50), self.__tamanho_botao[0], self.__tamanho_botao[1]])
-    
-    def rodar(self):
-        self.botoes()
-        self.mensagens()
-        
-        mouse = pygame.mouse.get_pos()
+    @property
+    def botao_recomecar(self):
+        return self.__botao_recomecar
 
-        for event in pygame.event.get():
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if self.__posicao_botao[0] + self.__tamanho_botao[0] > mouse[0] > self.__posicao_botao[0] and self.__posicao_botao[1] + 2 * self.__tamanho_botao[1] + 50 > mouse[1] > self.__posicao_botao[1] + self.__tamanho_botao[1] + 50:
-                    return False
-                elif self.__posicao_botao[0] + self.__tamanho_botao[0] > mouse[0] > self.__posicao_botao[0] and self.__posicao_botao[1] + 3 * self.__tamanho_botao[1] + 100 > mouse[1] > self.__posicao_botao[1] + 2 * (self.__tamanho_botao[1] + 50):
-                    pygame.quit()
-                    sys.exit()
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+    @property
+    def botao_sair(self):
+        return self.__botao_sair
