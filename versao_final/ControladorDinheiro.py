331fb99c8c1ed2b4fbab2b6f213793826559dc86
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-17 18:26:59 -0300

    Update ControladorDinheiro.py
    Uso da serialização para o dinheiro do jogador

--- versao_final/ControladorDinheiro.py
+++ versao_final/ControladorDinheiro.py
@@ -1,13 +1,15 @@
 from Sprites import *
+from VariaveisDao import VariaveisDAO
 
 
 class ControladorDinheiro:
     def __init__(self):
-        self.__dinheiro_jogador = 0
+        self.__dinheiro_jogador = VariaveisDAO().get('dinheiro')
 
     @property
     def dinheiro(self):
         return self.__dinheiro_jogador
+
     @dinheiro.setter
     def dinheiro(self, dinheiro):
         self.__dinheiro_jogador += dinheiro
