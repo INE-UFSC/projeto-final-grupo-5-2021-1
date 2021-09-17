19d274f98bd842c48cc6ad8485a65a874809a42f
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-16 23:44:22 -0300

    Update Configuracoes.py
    Código alterado para encaixar com o novo código View

--- versao_final/Configuracoes.py
+++ versao_final/Configuracoes.py
@@ -1,9 +1,13 @@
+from ConfiguracaoView import ConfiguracaoView
 import pygame
+import pygame_gui
+import sys
 
 class Configuracoes:
     def __init__(self):
         self.__volume_jogo = 3
         self.__volume_maximo = 5
+        self.__view_configuracao = ConfiguracaoView()
     
     def aumentar_volume(self):
         if self.__volume_jogo < self.__volume_maximo:
@@ -18,3 +22,24 @@ class Configuracoes:
     @property
     def volume_jogo(self):
         return self.__volume_jogo
+ 
+    def configuracao(self):
+        menu = True
+
+        while menu:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.USEREVENT:
+                    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                        if event.ui_element == self.__view_configuracao.botao_diminuir:
+                            self.diminuir_volume()
+                        elif event.ui_element == self.__view_configuracao.botao_aumentar:
+                            self.aumentar_volume()
+                        elif event.ui_element == self.__view_configuracao.botao_continuar:
+                            menu = False
+
+                self.__view_configuracao.ler_evento(event)
+                self.__view_configuracao.window(self.volume_jogo)
+
+            pygame.display.update()
