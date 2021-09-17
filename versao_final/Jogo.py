8ca1048f6ca8853c27669db3d2ce396df0064b78
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-17 00:01:11 -0300

    Update Jogo.py
    Adicionado elementos de serialização

--- versao_final/Jogo.py
+++ versao_final/Jogo.py
@@ -1,3 +1,5 @@
+from Configuracoes import Configuracoes
+from VariaveisDao import VariaveisDAO
 from Menu import MenuJogo
 from TelaJogo import TelaJogo
 from ControladorEstadoNivel import ControladorEstadoNivel
@@ -8,7 +10,7 @@ import random
 import sys
 from HUD import HUD
 from JogadorNave import Jogador
-from Inimigo import *
+# from Inimigo import *
 from Sprites import *
 from ControladorDinheiro import ControladorDinheiro
 from Loja import *
@@ -17,6 +19,7 @@ from LojaView import InterfaceLoja
 
 class Jogo:
     def __init__(self):
+        self.variaveis = VariaveisDAO()
         self.tempo_fase = 0
         self.tempo_maximo = 240
         self.FPS = 60
@@ -25,10 +28,11 @@ class Jogo:
         self.tela = TelaJogo()
         self.jogador = Jogador()
         self.controle = ControladorEstadoNivel()
-        self.controle_elementos = ControladorElementosNivel()
+        self.controle_elementos = ControladorElementosNivel(self.variaveis)
         self.controle_dinheiro = ControladorDinheiro()
         self.loja = Loja(self.jogador)
         self.interface_loja = InterfaceLoja()
+        self.configuracoes = Configuracoes()
 
     def elementos_tela(self):
         self.tela.mostrar_fundo()
@@ -40,7 +44,17 @@ class Jogo:
 
         sprites.jogador.draw(surface=self.tela.janela)
         sprites.jogador.sprite.tiros.draw(self.tela.janela)
+   
     def loop_jogo(self):
+        if MenuJogo().menu(self.configuracoes) == 'recomecar':
+            self.controle_elementos.nivel = 0
+            self.jogador.vida = 100
+            self.jogador.rect = Jogador().posicao
+
+        pygame.mixer.music.load("trilhasonora.wav")
+        pygame.mixer.music.set_volume(self.configuracoes.volume_jogo)
+        pygame.mixer.music.play(-1)
+   
         while True:
             self.tempo.tick(self.FPS)
             self.elementos_tela()
@@ -49,10 +63,15 @@ class Jogo:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_p:
-                        self.controle.pausar()
+                        if self.controle.pausar(self.configuracoes) == 'menu':
+                            self.controle_elementos.nivel = self.variaveis.get('nivel')
+                            self.jogador.vida = self.variaveis.get('vida')
+                            self.jogador.rect.center = (self.tela.largura/2, self.tela.altura - 100)
+                            self.jogador.tiros.empty()
+                            sprites.inimigos.empty()
+                            self.loop_jogo()
                 if event.type == pygame.QUIT:
                     sys.exit()
-
 
             self.controle_elementos.geracao_inimigos(self.tela.largura)
             self.controle_elementos.comportamento_inimigos(self.tela.janela, self.tela.altura,self.jogador, self.FPS)
@@ -63,6 +82,9 @@ class Jogo:
 
             if len(sprites.inimigos) == 0:
                 self.tempo_fase = 0
+                if self.controle_elementos.nivel != 0:
+                    self.controle.proxima_rodada(self.variaveis, self.controle_elementos.nivel, self.jogador.vida, self.jogador.escudo)
+        
             if self.tempo_fase >= self.tempo_maximo or self.jogador.vida <= 0:
                 self.controle_elementos.nivel = 0
                 self.jogador.tiros.empty()
@@ -75,6 +97,4 @@ class Jogo:
 
 
 jogo1 = Jogo()
-menu = MenuJogo()
-menu.menu()
 jogo1.loop_jogo()
