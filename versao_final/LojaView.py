e62db6535e3ac07fc80b3fc71baae4fced2858e0
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-22 22:19:26 -0300

    Update LojaView.py
    Adicionado texto para quando chega no limite da compra. Mudança no método window.

--- versao_final/LojaView.py
+++ versao_final/LojaView.py
@@ -23,6 +23,7 @@ class InterfaceLoja:
         self.atualizar(40)
 
         self.__saldo_insuficiente = False
+        self.__compra_maxima = False
 
     @property
     def botao_dano(self):
@@ -47,12 +48,26 @@ class InterfaceLoja:
     @saldo_insuficiente.setter
     def saldo_insuficiente(self, insuficiente: bool):
         self.__saldo_insuficiente = insuficiente
+        
+    @property
+    def compra_maxima(self):
+        return self.__compra_maxima
+
+    @compra_maxima.setter
+    def compra_maxima(self, maxima: bool):
+        self.__compra_maxima = maxima
 
     def fundos_insuficientes(self):
         fonte_mensagem = pygame.font.SysFont('comicssans', 40)
    
         mensagem_fundos_insuficientes = fonte_mensagem.render('Você não possui dinheiro suficiente!', True, (255,0,0))
         self.__tela.janela.blit(mensagem_fundos_insuficientes, (self.posicao_botao[0] + 30, self.tela.altura - 50))
+        
+    def mensagem_maximo_compra(self):
+        fonte_mensagem = pygame.font.SysFont('comicssans', 40)
+
+        mensagem_maximo_compra = fonte_mensagem.render('Você já comprou o máximo possível!', True, (255, 0, 0))
+        self.tela.janela.blit(mensagem_maximo_compra, (self.posicao_botao[0] + 30, self.tela.altura - 50))
 
     def textos(self, dinheiro: int, jogador: Jogador):
         fonte_mensagem = pygame.font.SysFont('comicssans', 70)
@@ -74,8 +89,8 @@ class InterfaceLoja:
         self.tela.janela.blit(valor_vida, (630, 90))
 
     def window(self, dinheiro: int, jogador: Jogador):
-        self.tela.mostrar_fundo()
+        super().window(dinheiro, jogador)
         if self.__saldo_insuficiente:
             self.mensagem_fundos_insuficientes()
-        self.textos(dinheiro, jogador)
-        super().window()
+        elif self.__compra_maxima:
+            self.mensagem_maximo_compra()
