c1ab6fcda47245fd22017ba58a3bd6d165683348
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-22 22:16:46 -0300

    Update Loja.py
    Adicionado limite de compra e serialização. Pausa na música quando está na loja.

--- versao_final/Loja.py
+++ versao_final/Loja.py
@@ -13,7 +13,11 @@ class Loja:
         self.__view_loja = InterfaceLoja()
         self.__custo_escudo = 100
         self.__custo_dano = 100
+        self.__maximo_compra_dano = 4
+        self.__compra_dano = LojaDAO().get('compradano')
         self.__custo_velocidade = 100
+        self.__maximo_compra_velocidade = 2
+        self.__compra_velocidade = LojaDAO().get('compravelocidade')
 
     def comprar_escudo(self, controlador_dinheiro: ControladorDinheiro):
         self.__jogador.vida += 25
@@ -27,9 +31,16 @@ class Loja:
         self.__jogador.velocidade += 1
         controlador_dinheiro.dinheiro -= self.__custo_velocidade
 
+    def compras_iniciais(self):
+        self.__compra_dano = 0
+        self.__compra_velocidade = 0
+
     def abrir_loja(self, controlador_dinheiro: ControladorDinheiro):
+        pygame.mixer.music.pause()
+
         loja = True
         self.__view_loja.saldo_insuficiente = False
+        self.__view_loja.compra_maxima = False
 
         while loja:
             for event in pygame.event.get():
@@ -39,23 +50,30 @@ class Loja:
                     if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                         if event.ui_element == self.__view_loja.botao_dano:
                             if controlador_dinheiro.dinheiro >= self.__custo_dano:
-                                self.aumentar_dano(controlador_dinheiro)
-                                VariaveisDAO().add('dano', self.__jogador.dano)
+                                if self.__maximo_compra_dano >= self.__compra_dano:
+                                    self.__compra_dano += 1
+                                    self.aumentar_dano(controlador_dinheiro)
+                                else:
+                                    self.__view_loja.compra_maxima = True
                             else:
                                 self.__view_loja.saldo_insuficiente = True
                         elif event.ui_element == self.__view_loja.botao_velocidade:
                             if controlador_dinheiro.dinheiro >= self.__custo_velocidade:
-                                self.aumentar_velocidade(controlador_dinheiro)
-                                VariaveisDAO().add('valocidade', self.__jogador.velocidade)
+                                if self.__maximo_compra_velocidade >= self.__compra_velocidade:
+                                    self.__compra_velocidade += 1
+                                    self.aumentar_velocidade(controlador_dinheiro)
+                                else:
+                                    self.__view_loja.compra_maxima = True
                             else:
                                 self.__view_loja.saldo_insuficiente = True
                         elif event.ui_element == self.__view_loja.botao_vida:
                             if controlador_dinheiro.dinheiro >= self.__custo_escudo:
                                 self.comprar_escudo(controlador_dinheiro)
-                                VariaveisDAO().add('vida', self.__jogador.vida)
                             else:
                                 self.__view_loja.saldo_insuficiente = True
                         elif event.ui_element == self.__view_loja.botao_continuar:
+                            LojaDAO().salvar_variaveis(self.__compra_dano, self.__compra_velocidade)
+                            pygame.mixer.music.unpause()
                             loja = False
 
                 self.__view_loja.ler_evento(event)
