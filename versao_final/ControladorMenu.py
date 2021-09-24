d0b269162dc40f9e00866932e096acc8d9f01536
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-24 04:45:41 -0300

    Add files via upload

---
+++ versao_final/ControladorMenu.py
@@ -0,0 +1,30 @@
+from Sprites import *
+from VariaveisDao import VariaveisDAO
+from ControladorDinheiro import ControladorDinheiro
+from Loja import Loja
+from JogadorNave import Jogador
+from ControladorElementosNivel import ControladorElementosNivel
+
+
+class ControladorMenu:
+    def __init__(self):
+        pass
+
+    def recomecar(self, controle: ControladorElementosNivel, jogador: Jogador, loja: Loja, dinheiro: ControladorDinheiro):
+        if isinstance(controle, ControladorElementosNivel) and isinstance(jogador, Jogador) and isinstance(loja, Loja) and isinstance(dinheiro, ControladorDinheiro):
+            controle.nivel = 0
+            controle.explodir = False
+            controle.tempo = 0
+            jogador.atributos_iniciais()
+            jogador.posicao_inicial()
+            loja.compras_iniciais()
+            dinheiro.zerar_dinheiro()
+
+    def voltar_menu(self, controle: ControladorElementosNivel, jogador: Jogador):
+        if isinstance(controle, ControladorElementosNivel) and isinstance(jogador, Jogador):
+            controle.nivel = VariaveisDAO().get('nivel')
+            controle.explodir = False
+            controle.tempo = 0
+            jogador.posicao_inicial()
+            jogador.tiros.empty()
+            sprites.inimigos.empty()
