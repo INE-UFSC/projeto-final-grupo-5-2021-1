c313fe3418559b26e3cdfb9d40cf2aa7e971ce5d
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-24 04:43:03 -0300

    Update ControladorEstadoNivel.py
    Mudança na implementação dos métodos.

--- versao_final/ControladorEstadoNivel.py
+++ versao_final/ControladorEstadoNivel.py
@@ -1,5 +1,13 @@
 from VariaveisDao import VariaveisDAO
+from ControladorDinheiro import ControladorDinheiro
+from ControladorElementosNivel import ControladorElementosNivel
+from ControladorMenu import ControladorMenu
+from Sprites import *
+from EfeitosSonoros import EfeitosSonoros
 from Configuracoes import Configuracoes
+from LojaDao import LojaDAO
+from Loja import Loja
+from AtributosDao import AtributosDAO
 from TelaJogo import TelaJogo
 from JogadorNave import Jogador
 import pygame
@@ -16,13 +24,21 @@ class ControladorEstadoNivel(pygame.sprite.Sprite):
         self.__view_fim_jogo = FimJogoView()
         self.__view_pausa = PausaView()
 
-    def fim_de_jogo(self, jogador: Jogador, tela: TelaJogo):
-        if isinstance(jogador, Jogador) and isinstance(tela, TelaJogo):
+    def fim_de_jogo(self, jogador: Jogador, tela: TelaJogo, loja: Loja, efeito_sonoro: EfeitosSonoros, controle: ControladorElementosNivel, dinheiro: ControladorDinheiro):
+        if isinstance(jogador, Jogador) and isinstance(tela, TelaJogo) and isinstance(loja, Loja) and isinstance(efeito_sonoro, EfeitosSonoros)
+            and isinstance(controle, ControladorElementosNivel) and isinstance(dinheiro, ControladorDinheiro):
+            controle.nivel = 0
+            jogador.tiros.empty()
+            sprites.inimigos.empty()
+            dinheiro.zerar_dinheiro()
             loja.compras_iniciais()
             jogador.atributos_iniciais()
             jogador.posicao_inicial()
+            VariaveisDAO().resetar_variaveis()
+            LojaDAO().resetar_variaveis()
 
-        pygame.mixer.music.pause()
+        efeito_sonoro.pausar_musica()
+        efeito_sonoro.tocar_som(efeito_sonoro.som_morte)
 
         morreu = True
 
@@ -33,7 +49,9 @@ class ControladorEstadoNivel(pygame.sprite.Sprite):
                 if event.type == pygame.USEREVENT:
                     if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
                         if event.ui_element == self.__view_fim_jogo.botao_recomecar:
-                            pygame.mixer.music.play(-1)
+                            efeito_sonoro.tocar_musica()
+                            controle.explodir = False
+                            controle.tempo = 0
                             morreu = False
                         elif event.ui_element == self.__view_fim_jogo.botao_sair:
                             self.__view_fim_jogo.confirmacao()
@@ -46,42 +64,42 @@ class ControladorEstadoNivel(pygame.sprite.Sprite):
 
             pygame.display.update()
 
-    def pausar(self, configuracoes: Configuracoes):
-        pygame.mixer.music.pause()
+    def pausar(self, configuracoes: Configuracoes, efeito_sonoro: EfeitosSonoros, controle: ControladorElementosNivel, jogador: Jogador, sprite: Sprites):
+        efeito_sonoro.pausar_musica()
    
         pausado = True
+        
+        if isinstance(configuracoes, Configuracoes) and isinstance(efeito_sonoro, EfeitosSonoros) and isinstance(controle, ControladorElementosNivel) and isinstance(jogador, Jogador):
+            while pausado:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+                    if event.type == pygame.USEREVENT:
+                        if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                            if event.ui_element == self.__view_pausa.botao_continuar:
+                                efeito_sonoro.despausar_musica()
+                                pausado = False
+                            elif event.ui_element == self.__view_pausa.botao_configurar:
+                                try:
+                                    configuracoes.configuracao(efeito_sonoro)
+                                except AttributeError:
+                                    print('Não possui esse método')
+                            elif event.ui_element == self.__view_pausa.botao_menu:
+                                ControladorMenu().voltar_menu(controle, jogador)
+                                return 'menu'
 
-        while pausado:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.type == pygame.USEREVENT:
-                    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                        if event.ui_element == self.__view_pausa.botao_continuar:
-                            pygame.mixer.music.unpause()
-                            pausado = False
-                        elif event.ui_element == self.__view_pausa.botao_configurar:
-                            try:
-                                configuracoes.configuracao()
-                            except AttributeError:
-                                print('Não possui esse método')
-                        elif event.ui_element == self.__view_pausa.botao_menu:
-                            return 'menu'
+                    self.__view_pausa.ler_evento(event)
+                    self.__view_pausa.window()
 
-                self.__view_pausa.ler_evento(event)
-                self.__view_pausa.window()
+                pygame.display.update()
 
-            pygame.display.update()
-
-    def proxima_rodada(self, dao: VariaveisDAO(), nivel: int, dinheiro: int, jogador: Jogador):
-        if isinstance(dao, VariaveisDAO) and isinstance(jogador, Jogador):
-            dao.add('nivel', nivel)
-            if dao.get('vida') != jogador.vida:
-                dao.add('vida', jogador.vida)
-            if dao.get('velocidade') != jogador.velocidade:
-                dao.add('velocidade', jogador.velocidade)
-            if dao.get('dano') != jogador.dano:
-                dao.add('dano', jogador.dano)
-            if dao.get('dinheiro') != dinheiro:
-                dao.add('dinheiro', dinheiro)
-
+    def proxima_rodada(self, dao: VariaveisDAO, controle: ControladorElementosNivel, dinheiro: ControladorDinheiro, jogador: Jogador, loja: Loja, efeito_sonoro: EfeitosSonoros):
+        if isinstance(dao, VariaveisDAO) and isinstance(jogador, Jogador) and isinstance(controle, ControladorElementosNivel) and isinstance(dinheiro, ControladorDinheiro)
+            and isinstance(loja, Loja) and isinstance(efeito_sonoro, EfeitosSonoros):
+            dao.salvar_variaveis(jogador.vida, jogador.dano, jogador.velocidade, dinheiro.dinheiro, controle.nivel)
+            jogador.tiros.empty()
+            if controle.nivel != 0:
+                controle.explodir = False
+                controle.tempo = 0
+                loja.abrir_loja(dinheiro, efeito_sonoro)
+                jogador.posicao_inicial()
