14161e0587b2ac8948f0465704ee912baa023070
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-24 05:19:26 -0300

    Update Menu.py
    Uso da classe ControladorMenu.

--- versao_final/Menu.py
+++ versao_final/Menu.py
@@ -1,3 +1,10 @@
+from VariaveisDao import VariaveisDAO
+from Loja import Loja
+from ControladorDinheiro import ControladorDinheiro
+from JogadorNave import Jogador
+from ControladorElementosNivel import ControladorElementosNivel
+from ControladorMenu import ControladorMenu
+from EfeitosSonoros import EfeitosSonoros
 from MenuView import MenuView
 from Configuracoes import Configuracoes
 import pygame
@@ -13,31 +20,34 @@ class MenuJogo(pygame.sprite.Sprite):
         self.is_on_menu = True
         self.__view_menu = MenuView()
     
-    def menu(self, configuracoes: Configuracoes):
-        while self.is_on_menu:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.type == pygame.USEREVENT:
-                    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-                        if event.ui_element == self.__view_menu.botao_iniciar:
-                            self.is_on_menu = False
-                            return 'recomecar'
-                        elif event.ui_element == self.__view_menu.botao_continuar:
-                            self.is_on_menu = False
-                        elif event.ui_element == self.__view_menu.botao_configurar:
-                            try:
-                                configuracoes.configuracao()
-                            except AttributeError:
-                                print('Não possui esse método')
-                        elif event.ui_element == self.__view_menu.botao_sair:
-                            self.__view_menu.confirmacao()
-                    if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
-                        pygame.quit()
+    def menu(self, configuracoes: Configuracoes, efeitos_sonoros: EfeitosSonoros, controle: ControladorElementosNivel, jogador: Jogador, loja: Loja, dinheiro: ControladorDinheiro):
+        if isinstance(configuracoes, Configuracoes) and isinstance(efeitos_sonoros, EfeitosSonoros) and isinstance(controle, ControladorElementosNivel)
+            and isinstance(jogador, Jogador) and isinstance(loja, Loja) and isinstance(dinheiro, ControladorDinheiro):
+            while self.is_on_menu:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
                         sys.exit()
+                    if event.type == pygame.USEREVENT:
+                        if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                            if event.ui_element == self.__view_menu.botao_iniciar:
+                                self.is_on_menu = False
+                                ControladorMenu().recomecar(controle, jogador, loja, dinheiro)
+                                VariaveisDAO().resetar_variaveis()
+                            elif event.ui_element == self.__view_menu.botao_continuar:
+                                self.is_on_menu = False
+                            elif event.ui_element == self.__view_menu.botao_configurar:
+                                try:
+                                    configuracoes.configuracao()
+                                except AttributeError:
+                                    print('Não possui esse método')
+                            elif event.ui_element == self.__view_menu.botao_sair:
+                                self.__view_menu.confirmacao()
+                        if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
+                            pygame.quit()
+                            sys.exit()
 
-                self.__view_menu.ler_evento(event)
-                self.__view_menu.window()
+                    self.__view_menu.ler_evento(event)
+                    self.__view_menu.window()
 
-            pygame.display.update()
+                pygame.display.update()
    
