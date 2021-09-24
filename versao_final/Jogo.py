564d89d39c032b19ab5501c757bca39187e10124
marianamheitor <85947819+marianamheitor@users.noreply.github.com>
2021-09-24 05:31:42 -0300

    Update Jogo.py
    Diminuição de responsabilidade.

--- versao_final/Jogo.py
+++ versao_final/Jogo.py
@@ -1,24 +1,25 @@
+from EfeitosSonoros import EfeitosSonoros
 from Configuracoes import Configuracoes
-from VariaveisDao import VariaveisDAO
+from AtributosDao import AtributosDAO
 from Menu import MenuJogo
 from TelaJogo import TelaJogo
 from ControladorEstadoNivel import ControladorEstadoNivel
 from ControladorElementosNivel import ControladorElementosNivel
-# from Inimigo import Inimigo
 import pygame
-import random
 import sys
 from HUD import HUD
 from JogadorNave import Jogador
-# from Inimigo import *
 from Sprites import *
 from ControladorDinheiro import ControladorDinheiro
-from Loja import *
+from Loja import Loja
 
+
+pygame.init()
+pygame.display.set_caption('Space Conqueror')
 
 class Jogo:
     def __init__(self):
-        self.variaveis = VariaveisDAO()
+        self.atributos = AtributosDAO()
         self.tempo_fase = 0
         self.tempo_maximo = 240
         self.FPS = 60
@@ -27,10 +28,11 @@ class Jogo:
         self.tela = TelaJogo()
         self.jogador = Jogador()
         self.controle = ControladorEstadoNivel()
-        self.controle_elementos = ControladorElementosNivel(self.variaveis)
+        self.controle_elementos = ControladorElementosNivel(self.atributos, self.controle_dinheiro)
         self.controle_dinheiro = ControladorDinheiro()
         self.loja = Loja(self.jogador)
         self.configuracoes = Configuracoes()
+        self.efeitos_sonoros = EfeitosSonoros()
 
     def elementos_tela(self):
         self.tela.mostrar_fundo()
@@ -44,55 +46,43 @@ class Jogo:
         sprites.jogador.sprite.tiros.draw(self.tela.janela)
    
     def loop_jogo(self):
-        if MenuJogo().menu(self.configuracoes) == 'recomecar':
-            self.controle_elementos.nivel = 0
-            self.jogador.vida = 100
-            self.jogador.dano = 25
-            self.jogador.velocidade = 5
-            self.controle_dinheiro.dinheiro = 0
+        MenuJogo().menu(self.configuracoes, self.efeitos_sonoros, self.controle_elementos, self.jogador, self.loja, self.controle_dinheiro)
 
-        pygame.mixer.music.load("trilhasonora.wav")
-        pygame.mixer.music.set_volume(self.configuracoes.volume_jogo)
-        pygame.mixer.music.play(-1)
+        self.efeitos_sonoros.carregar_musica("trilhasonora.wav")
+        
+        pygame.mixer.music.set_volume(self.configuracoes.volume_jogo/10)
+        for som in self.efeitos_sonoros.sons:
+            som.set_volume(self.configuracoes.volume_jogo/10)
+
+        self.efeitos_sonoros.tocar_musica()
    
         while True:
             self.tempo.tick(self.FPS)
             self.elementos_tela()
-            sprites.jogador.update()
+            self.controle_elementos.explosao(self.tela.janela)
+            sprites.jogador.update(self.efeitos_sonoros)
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_p:
-                        if self.controle.pausar(self.configuracoes) == 'menu':
-                            self.controle_elementos.nivel = self.variaveis.get('nivel')
-                            self.jogador.vida = self.variaveis.get('vida')
-                            self.jogador.rect.center = (self.tela.largura/2, self.tela.altura - 100)
-                            self.jogador.tiros.empty()
-                            sprites.inimigos.empty()
+                        if self.controle.pausar(self.configuracoes, self.efeitos_sonoros, self.controle_elementos, self.jogador) == 'menu':
                             self.loop_jogo()
                 if event.type == pygame.QUIT:
                     sys.exit()
 
             self.controle_elementos.geracao_inimigos(self.tela.largura)
             self.controle_elementos.comportamento_inimigos(self.tela.janela, self.tela.altura,self.jogador, self.FPS)
-            self.controle_elementos.colisoes(self.jogador, self.controle_dinheiro)
+            self.controle_elementos.colisoes(self.jogador, self.efeitos_sonoros)
             self.tempo_fase += 1 / self.FPS
             self.controle_dinheiro.gerar_recompensa()
 
-            if len(sprites.inimigos) == 0:
-                self.jogador.tiros.empty()
-                self.loja.abrir_loja(self.controle_dinheiro)
-                self.tempo_fase = 0
-                if self.controle_elementos.nivel != 0:
-                    self.controle.proxima_rodada(self.variaveis, self.controle_elementos.nivel, self.jogador.vida, self.controle_dinheiro.dinheiro)
-        
             if self.tempo_fase >= self.tempo_maximo or self.jogador.vida <= 0:
-                self.controle_elementos.nivel = 0
-                self.jogador.tiros.empty()
-                sprites.inimigos.empty()
                 self.tempo_fase = 0
-                self.controle_dinheiro.zerar_dinheiro()
-                self.controle.fim_de_jogo(self.jogador, self.tela)
+                self.controle.fim_de_jogo(self.jogador, self.tela, self.loja, self.efeitos_sonoros, self.controle_elementos, self.controle_dinheiro)
+
+            if len(sprites.inimigos) == 0:
+                self.tempo_fase = 0
+                self.controle.proxima_rodada(self.variaveis, self.controle_elementos, self.controle_dinheiro, self.jogador, self.loja, self.efeitos_sonoros)
 
             pygame.display.update()
 
